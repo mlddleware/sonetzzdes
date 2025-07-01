dacaac7e63ed90b9aefd0af86708220a756c3b9f
middleware <130544221+mlddleware@users.noreply.github.com>
2025-07-01 21:07:18 +0500

    Fix Render deployment: simplify build.sh, add Procfile, clean render.yaml

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn fastlove.wsgi:application 
--- build.sh
+++ build.sh
@@ -6,11 +6,6 @@ set -o errexit  # exit on error
 
 echo "=== Render Build Script ==="
 
-# Install dependencies
-echo "Installing Python dependencies..."
-pip install --upgrade pip
-pip install -r requirements.txt
-
 # Collect static files
 echo "Collecting static files..."
 python manage.py collectstatic --no-input
